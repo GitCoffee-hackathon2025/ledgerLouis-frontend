4a9b9cf3167f69b6b63efa8546ee5c335a8adc59
Chormiak <william.chormi@gmail.com>
2026-03-13 09:29:20 -0300

    FEAT: Resentando as página do vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+/* Recomendo usar grid para a criação dos layouts */
+</style>
