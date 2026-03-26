16114897f8d8badc5541569f0eabd3efa3bbefcf
MuriloJamoski <mugames2002@gmail.com>
2026-03-26 11:24:59 -0300

    FEAT: configuraçao inicial do PWA

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { registerSW } from 'virtual:pwa-register';
 
 import App from './App.vue';
 import router from './router';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // opcional: notificar usuário para atualizar app
+    console.log('Nova versão disponível.');
+  },
+  onOfflineReady() {
+    console.log('App PWA está pronto offline.');
+  }
+});
 
 const app = createApp(App);
 
@@ -12,3 +23,5 @@ app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
+
+export { updateSW };
