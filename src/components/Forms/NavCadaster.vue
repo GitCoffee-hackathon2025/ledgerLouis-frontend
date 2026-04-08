df550caca70cdb50b387a8fab5deb872adf81442
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2026-04-08 15:17:28 -0300

    Feat: inicio Nav Cadaster

---
+++ src/components/Forms/NavCadaster.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+<template>
+<nav>
+    <ul>
+        <li>Entrar</li>
+        <li><div></div></li>
+        <li>registrar</li>
+    </ul>
+</nav>
+</template>
+<style scoped>
+ul{
+    background-color: #1DE276;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+</style>
---
+++ src/components/inputs/Button.vue
@@ -0,0 +1,3 @@
+<template>
+    <button>Cadastro</button>    
+</template>
