3c0d52e89a385a09eaba659eed8a807f5222be2b
MuriloJamoski <mugames2002@gmail.com>
2026-04-08 15:17:50 -0300

    REFACTORE:  arrumando nav e sistemas de adicionar entradas e saidas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import BottomNav from './components/BottomNav.vue'
 </script>
 
 <template>
+<BottomNav />
   <RouterView />
 </template>
 
