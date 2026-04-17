bd9dcf79e91aa10bcaa6e6cf31b6b10a7384c2f3
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2026-04-17 18:06:40 -0300

    FEAT: pronto para integração

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import {} from 'vue-router'
-
-import FormView from './views/FormView.vue';
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <FormView/>
+  <RouterView />
 </template>
 
 <style scoped>
 /* Recomendo usar grid para a criação dos layouts - William */
-
 </style>
